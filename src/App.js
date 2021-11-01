7f51545dc5724f0d6dfaf5b7848724231295c6e1
KASAYED <kasayeddev.com>
2021-11-01 20:05:10 +0600

    1. React Framer Motion setUup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import black from "./black.png";
+import blue from "./blue.png";
+import { Card, CardGrid, Container, Header } from "./Elements";
+import green from "./green.png";
+import Menu from "./Menu";
+import purp from "./purp.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Menu />
+        <h1>Header</h1>
+      </Header>
+      <Container>
+        <h2>Super Cool</h2>
+        <CardGrid>
+          <Card style={{ background: "var(--purp)" }}>
+            <h3>Some card</h3>
+            <img src={purp} />
+          </Card>
+          <Card style={{ background: "var(--blue)" }}>
+            <h3>Some card</h3>
+            <img src={blue} />
+          </Card>
+          <Card style={{ background: "var(--black)" }}>
+            <h3>Some card</h3>
+            <img src={black} />
+          </Card>
+          <Card style={{ background: "var(--green)" }}>
+            <h3>Some card</h3>
+            <img src={green} />
+          </Card>
+        </CardGrid>
+      </Container>
     </div>
   );
 }
